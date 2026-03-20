7cfb5961f329b92a0b99b8e68218bdb0c2672476
anchieta.junior <santosjr87@gmail.com>
2026-03-19 22:08:55 -0300

    Add security config class

---
+++ src/main/java/com/example/favoritosapi/model/Usuario.java
@@ -0,0 +1,40 @@
+package com.example.favoritosapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 200)
+    private String email;
+
+    @Column(nullable = false, length = 200)
+    private String senha;
+
+}
